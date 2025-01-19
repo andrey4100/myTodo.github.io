07a48846f3bf3cdb068f66d8e8c35bf593e9d040
Andrey <andronicuss@yandex.ru>
2025-01-19 16:33:29 +0300

    message

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,90 @@
-import TodoList from '../todo-list';
-import AppHeader from '../app-header';
-import SearchPanel from '../search-panel';
-import ItemStatusFilter from '../item-status-filter';
+import React, { Component } from 'react';
 
-import './app.css';
+import TaskList from '../TaskList';
+import NewTaskForm from '../NewTaskForm';
+import Footer from '../Footer';
 
-const App = () => {
+import './App.css';
 
-  const todoData = [
-    { label: "Drink Coffe", important: false, id: 1 },
-    { label: "Make Awesome App", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 },
-  ];
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      filter: 'All',
+    };
+  }
 
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={ todoData }/>
-    </div>
-  );
-};
+  addItem(value) {
+    const data = {
+      id: this.state.todos.length + 1,
+      body: value,
+      checked: false,
+      date: new Date(),
+    };
+    this.setState(({ todos }) => ({ todos: [...todos, data] }));
+  }
+
+  deleteItem(ident) {
+    this.setState(({ todos }) => ({
+      todos: todos.filter(({ id }) => id !== ident),
+    }));
+  }
+
+  changeCheck(ident, data) {
+    this.setState(({ todos }) => ({
+      todos: todos.map((element) => {
+        if (ident === element.id) element.checked = data;
+        return element;
+      }),
+    }));
+  }
+
+  editItem(ident, text) {
+    this.setState(({ todos }) => ({
+      todos: todos.map((element) => {
+        if (element.id === ident) element.body = text;
+        return element;
+      }),
+    }));
+  }
+
+  filteredItems() {
+    const { todos, filter } = this.state;
+    return todos.filter(({ checked }) => {
+      const all = filter === 'All';
+      const completed = filter === 'Completed';
+      return all ? true : completed ? checked === true : checked === false;
+    });
+  }
+
+  clearCompleted() {
+    this.setState(({ todos }) => ({ todos: todos.filter((element) => !element.checked) }));
+  }
+
+  changeFilter(data) {
+    this.setState({ filter: data });
+  }
+
+  render() {
+    return (
+      <div className="todoapp">
+        <NewTaskForm title="Todos" placeholder="What needs to be done?" addItem={this.addItem.bind(this)} />
+        <TaskList
+          changeCheck={this.changeCheck.bind(this)}
+          editItem={this.editItem.bind(this)}
+          deleteItem={this.deleteItem.bind(this)}
+          todos={this.filteredItems()}
+        />
+        <Footer
+          changeFilter={this.changeFilter.bind(this)}
+          clearCompleted={this.clearCompleted.bind(this)}
+          lefts={this.state.todos.filter(({ checked }) => !checked).length}
+          filter={this.state.filter}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
