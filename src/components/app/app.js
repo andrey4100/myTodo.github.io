ba9b6f378c0266b48bc2a9bfb742cb85448a28b7
Andrey <andronicuss@yandex.ru>
2025-01-31 22:09:55 +0300

    test lint

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,91 +7,83 @@ import Footer from '../Footer';
 import './App.css';
 
 class App extends Component {
-
   // Инициализируем состояние компонента
   state = {
     todos: [
-        { id: 1, label: 'Изучить HTML, CSS', completed: true, editing: false, time: new Date() },
-        { id: 2, label: 'Изучить JS', completed: true, editing: false, time: new Date() }
-        { id: 3, label: 'Изучить React', completed: false, editing: false, time: new Date() },
+      { id: 1, label: 'Изучить HTML, CSS', completed: true, editing: false, time: new Date() },
+      { id: 2, label: 'Изучить JS', completed: true, editing: false, time: new Date() },
+      { id: 3, label: 'Изучить React', completed: false, editing: false, time: new Date() },
     ],
-    filter: 'all'
-};
+    filter: 'all',
+  };
 
-    // Метод для добавления новой задачи (стрелочная функция, чтобы не ипользовать "bind")
-    addTask = (label) => {
-      const newTask = {
-        id: Date.now(),
-        label,
-        completed: false,
-        editing: false,
-        time: new Date()
-      };
-      this.setState(prevState => ({ todos: [...prevState.todos, newTask] }));
+  // Метод для добавления новой задачи (стрелочная функция, чтобы не ипользовать "bind")
+  addTask = (label) => {
+    const newTask = {
+      id: Date.now(),
+      label,
+      completed: false,
+      editing: false,
+      time: new Date(),
     };
+    this.setState((prevState) => ({ todos: [...prevState.todos, newTask] }));
+  };
 
-    // Метод для переключения статуса выполнения задачи
+  // Метод для переключения статуса выполнения задачи
   toggleCompleted = (id) => {
-    this.setState(prevState => ({
-      todos: prevState.todos.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)),
     }));
   };
 
-    // Метод для переключения статуса редактирования задачи
+  // Метод для переключения статуса редактирования задачи
   editTask = (id) => {
-    this.setState(prevState => ({
-        todos: prevState.todos.map(todo =>
-            todo.id === id ? {...todo, editing: !todo.editing} : todo
-        )
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => (todo.id === id ? { ...todo, editing: !todo.editing } : todo)),
     }));
   };
 
   // Метод для удаления задачи
   deleteTask = (id) => {
-    this.setState(prevState => ({ todos: prevState.todos.filter(todo => 
-        todo.id !== id) }));
+    this.setState((prevState) => ({ todos: prevState.todos.filter((todo) => todo.id !== id) }));
   };
 
   // Метод для изменения текста таски
-  changeTaskLabel = (id, newLabel) => this.setState(prevState => ({
-        todos: prevState.todos.map(todo =>
-            todo.id === id ? {...todo, label: newLabel} : todo
-        )
+  changeTaskLabel = (id, newLabel) =>
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => (todo.id === id ? { ...todo, label: newLabel } : todo)),
     }));
 
   // Метод для изменения фильтра
   setFilter = (filter) => {
     this.setState({ filter });
   };
-  
+
   // Метод для очистки завершенных задач
   removeCompletedTasks = () => {
-    this.setState(prevState => ({ todos: prevState.todos.filter(todo => !todo.completed) }));
+    this.setState((prevState) => ({ todos: prevState.todos.filter((todo) => !todo.completed) }));
   };
 
-    // Метод для фильтрации задач
-    getFilteredTodos = () => {
-      const { todos, filter } = this.state;
-      if (filter === 'active') {
-          return todos.filter(todo => !todo.completed);
-      }
-      if (filter === 'completed') {
-        return todos.filter(todo => todo.completed);
-      } 
-      return todos;
+  // Метод для фильтрации задач
+  getFilteredTodos = () => {
+    const { todos, filter } = this.state;
+    if (filter === 'active') {
+      return todos.filter((todo) => !todo.completed);
+    }
+    if (filter === 'completed') {
+      return todos.filter((todo) => todo.completed);
+    }
+    return todos;
   };
 
   render() {
-
     const filteredTodos = this.getFilteredTodos();
 
     return (
       <section className="todoapp">
         <header className="header">
           <h1>Todos</h1>
-          <NewTaskForm onAddTask={this.addTask}/>
+          <NewTaskForm onAddTask={this.addTask} />
         </header>
         <section className="main">
           <TaskList
@@ -102,10 +94,10 @@ class App extends Component {
             onLabelChange={this.changeTaskLabel}
           />
           <Footer
-          todos={this.state.todos}
-          filter={this.state.filter}
-          onFilterChange={this.setFilter}
-          onClearCompleted={this.removeCompletedTasks}
+            todos={this.state.todos}
+            filter={this.state.filter}
+            onFilterChange={this.setFilter}
+            onClearCompleted={this.removeCompletedTasks}
           />
         </section>
       </section>
